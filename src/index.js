67801e7d2616bf4ed2a3db3e9f4304e364ba85ec
Sharfunisa Shajahan <sharfunisa.sa@gmail.com>
2026-07-02 08:54:15 +0530

    Refactor App component and clean up imports

--- src/index.js
+++ src/index.js
@@ -5,54 +5,23 @@ import Login from './pages/login'
 import Signup from './pages/signup';
 import Landing from './pages/landing'
 
-import { BrowserRouter,Link,Route, Routes } from 'react-router-dom';
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App(){
-
-  const[users,setusers] = useState(
-    [
-      {
-        username:"nisha",
-        password:"123"
-      }
-    ]
-  )
-
+  const [users, setusers] = useState([
+    { username: "nisha", password: "123" }
+  ])
 
   return(
-  <BrowserRouter>
-    
-  <Routes>
-    <Route path='/' element={<Login users={users} setusers={setusers}/>}>
-
-    </Route>
-
-    <Route path='/signup' element={<Signup users={users} setusers={setusers}/>}>
-
-    </Route>
-    <Route path='/landing' element={<Landing/>}>
-
-    </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login users={users} setusers={setusers}/>} />
+        <Route path='/signup' element={<Signup users={users} setusers={setusers}/>} />
+        <Route path='/landing' element={<Landing/>} />
+      </Routes>
     </BrowserRouter>
-    )
+  )
 }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-   
-   
-   
-  <App></App>
-
-   </div>
-
-  
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+root.render(<App></App>);
